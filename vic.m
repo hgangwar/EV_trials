b38d67fa921df19db2b69ae370f4edb6073c5de7
Harsh Gangwar <121974220+hgangwar@users.noreply.github.com>
2024-03-11 10:05:08 -0700

    .89 percent

--- vic.m
+++ vic.m
@@ -1,4 +1,11 @@
+base = load("baseline.mat");
+%mpc = load("NMPC_ts200_P2sec_delta_u_without_J2.mat");
+%mpc = load("NMPC_ts200_P2sec_delta_u_ts_sqp_leg_init_2.mat");
+mpc = load("NMPC_ts200_P2sec_delta_u_ts_sqp_leg_init.mat");
+base_SOC=base.out.baseline_SOC;
+mpc_SOC = mpc.out.SOC;
 SOC_used=60-base_SOC(end);
 savings=mpc_SOC(end)-base_SOC(end);
 percentage=(savings/SOC_used)*100;
 disp(percentage)
+scatter(step,mpc.out.prev_u, 'r');
