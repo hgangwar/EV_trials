b2e1220794bffaac59ea22518b1bd4d4e0cfbc37
Harsh Gangwar <121974220+hgangwar@users.noreply.github.com>
2024-03-19 13:53:32 -0700

    wltp3

--- global_init.m
+++ global_init.m
@@ -1,9 +1,10 @@
 
-Data1=load("ftp75_5ms.mat");
+%Data1=load("ftp75_5ms.mat");
+load("ds_wltp3_5ms.mat");
 Data2 = load("eff_map.mat");
 Data3 = load("Max_Torque_vs_EMSpeed.mat");
 
-custom_struct.ftp75_5ms = Data1.ftp75_5ms;
+custom_struct.ftp75_5ms = wltp3_5ms;
 custom_struct.eff = Data2.eff;
 custom_struct.torque = Data2.torque;
 custom_struct.speed = Data2.speed;
--- vic.m
+++ vic.m
@@ -1,4 +1,11 @@
-base = load("baseline.mat");
+%base = load("baseline.mat");
+base = load("baseline_wtp3.mat");
+base_velocity = base.out.velocity;
+base_Trqcmd_1 = base.out.Trqcmd(:,1);
+base_Trqcmd_2 = base.out.Trqcmd(:,2);
+base_TTrq= base_Trqcmd_1+base_Trqcmd_2;
+base_decision= base_Trqcmd_1./base_TTrq;
+
 %mpc = load("NMPC_ts200_P2sec_delta_u_without_J2.mat");
 %mpc = load("NMPC_ts200_P2sec_delta_u_ts_sqp_leg_init_2.mat");
 %mpc = load("NMPC_ts200_P2sec_delta_u_ts_sqp_leg_init.mat");
@@ -8,7 +15,13 @@ base = load("baseline.mat");
 %mpc = load("NMPC_mod.mat");
 %mpc = load("NMPC_ts200_P2sec_delta_u_feedback_with_j2.mat");
 %mpc = load("NMPC_ts200_P2sec_delta_u_feedback_blind.mat");
-mpc = load("NMPC_ts400_P4sec_delta_u_feedback_sqp_Pdriver.mat");
+%mpc = load("NMPC_ts400_P4sec_delta_u_feedback_sqp_Pdriver.mat");
+mpc = load("NMPC_ts200_P2sec_delta_u_feedback_blind_wtp3.mat");
+mpc_velocity = mpc.out.velocity;
+mpc_Trqcmd_1 = mpc.out.Trqcmd(:,1);
+mpc_Trqcmd_2 = mpc.out.Trqcmd(:,2);
+mpc_TTrq=mpc_Trqcmd_1+mpc_Trqcmd_2;
+mpc_decision = mpc_Trqcmd_1./mpc_TTrq;
 
 base_SOC=base.out.baseline_SOC;
 mpc_SOC = mpc.out.SOC;
@@ -17,11 +30,9 @@ savings=mpc_SOC(end)-base_SOC(end);
 percentage=(savings/SOC_used)*100;
 disp(percentage)
 step=1:247401;
-figure(3)
-<<<<<<< Updated upstream
-scatter(step,mpc.out.prev_u, 'r');
+line_width=5;
+scatter(step,mpc_decision,line_width,'filled', 'r');
+hold on;
+scatter(step,base_decision,line_width,'b')
 %performance=[performance; (percentage)];
-=======
-scatter(step,mpc.out.prev_u, 10,'r');
-%performance=[performance; (percentage)];n
->>>>>>> Stashed changes
+
